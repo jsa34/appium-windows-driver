24af8ecedb528b68e79aea168fa1ed93db50878c
Jason Allen <jsa34@cam.ac.uk>
2020-04-02 14:18:41 +0100

    More fixes / tidy up and test added

--- test/e2e/driver-e2e-specs.js
+++ test/e2e/driver-e2e-specs.js
@@ -46,11 +46,34 @@ describe('Driver', function () {
     title.should.eql('Untitled - Notepad');
     const setClipboardTestText = 'Set Clipboard Test';
     //Set test text
-    await driver.setClipboard(setClipboardTestText, 'plaintext');
+    await driver.setClipboard(Buffer.from(setClipboardTestText, 'utf8').toString('base64'), 'plaintext');
     //Paste text
     await driver.elementByClassName('Edit').sendKeys('\uE009' + 'v');
     const pastedText = await driver.elementByClassName('Edit').text();
     pastedText.should.eql(setClipboardTestText);
+    await driver.elementByClassName('Edit').clear();
+  });
+
+  it('should copy text to the clipboard', async function () {
+    await driver.init({
+      app: 'C:\\Windows\\System32\\notepad.exe',
+      platformName: 'Windows',
+      deviceName: 'WindowsPC'
+    });
+    const title = await driver.title();
+    title.should.eql('Untitled - Notepad');
+    const getClipboardTestText = 'Get Clipboard Test';
+    //Type test text
+    await driver.elementByClassName('Edit').sendKeys(getClipboardTestText);
+    //Copy to clipboard
+    await driver.elementByClassName('Edit').sendKeys('\uE009' + 'a');
+    await driver.elementByClassName('Edit').sendKeys('\uE009' + 'x');
+    //Compare clipboard text to original text
+    const originalTextBaseSf = await (Buffer.from(getClipboardTestText, 'utf8').toString('base64'));
+    const clipboardText = await driver.getClipboard('plaintext');
+    //Notepad adds carriage returns to clipboard text
+    const trimmedClipboardText = clipboardText.replace(/[\r\n]/g, '');
+    trimmedClipboardText.should.eql(originalTextBaseSf);
   });
 
 });
